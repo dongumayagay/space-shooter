06d663302b1dd90206d176bcbc972320483713f5
tontingkaloy <51516536+tontingkaloy@users.noreply.github.com>
2020-07-13 16:24:57 +0800

    addition of enemy and laser to enemy collision

--- Space_shooter_console.py
+++ Space_shooter_console.py
@@ -1,40 +1,70 @@
-import curses.ascii
+import curses.ascii, random, time, itertools
 
+# initializations
 screen = curses.initscr()
 screen.nodelay(True)
 screen.keypad(True)
 screen.timeout(100)
 curses.curs_set(0)
-
 sh, sw = screen.getmaxyx()
-
-ship_y, ship_x = int(sh * .9), sw // 2
-ship_speed = 2
+ship_y, ship_x = int(sh * .95), sw // 2
+ship_speed = 1
 lasers = []
-
+enemies = []
+start_time = time.time()
+move_time = time.time()
 
 while True:
+
+    # controls
     key = screen.getch()
     if key == curses.ascii.ESC:
         curses.endwin()
         quit()
-    if key == curses.KEY_LEFT:
+    if key == curses.KEY_LEFT and ship_x - 2 > 0:
         ship_x -= ship_speed
-    if key == curses.KEY_RIGHT:
+    if key == curses.KEY_RIGHT and ship_x + 2 < sw - 1:
         ship_x += ship_speed
     if key == ord(' '):
-        lasers.append([ship_y-2, ship_x])
+        lasers.append([ship_y - 2, ship_x])
 
-
-
-
+    # drawing and logic
     screen.clear()
-    screen.addch(ship_y-1, ship_x, '#')
-    screen.addstr(ship_y, ship_x-2,'#####')
+
+    # ship drawing
+    screen.addch(ship_y - 1, ship_x, '#')
+    screen.addstr(ship_y, ship_x - 2, '#####')
+
+    # laser drawing and movement
     for laser in lasers:
-        screen.addch(laser[0],laser[1],'*')
+        screen.addch(laser[0], laser[1], '*')
         if laser[0] <= 2:
             lasers.remove(laser)
         else:
             laser[0] -= 1
+
+    # enemy spawn
+    if time.time() - start_time >= 3:
+        start_time = time.time()
+        enemies.append([1, random.randint(2, sw - 3)])
+
+    # enemy drawing
+    for enemy in enemies:
+        screen.addch(enemy[0], enemy[1], 'O')
+        if enemy[0] >= sh - 2:
+            enemies.remove(enemy)
+
+    # enemy movement (i created this because enemy is so fast)
+    if time.time() - move_time >= 0.3 and len(enemies) > 0:
+        move_time = time.time()
+        for enemy in enemies:
+            enemy[0] += 1
+
+    # collision
+    for enemy in enemies:
+        for laser in lasers:
+            if enemy[0] == laser[0] and enemy[1] == laser[1]:
+                enemies.remove(enemy)
+                lasers.remove(laser)
+
     screen.refresh()
